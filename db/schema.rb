f0d42fe97d92d28096ec822b27ab3a480165d204
Hakeem Baba <keemtaker@yahoo.com>
2020-02-23 23:47:39 +0100

    set up of company and job models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_175837) do
+ActiveRecord::Schema.define(version: 2020_02_23_223611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "website"
+    t.string "address"
+    t.string "location"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_companies_on_user_id", unique: true
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.string "role"
+    t.text "description"
+    t.string "job_type"
+    t.string "keywords"
+    t.string "salary"
+    t.string "location"
+    t.bigint "company_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_jobs_on_company_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -36,4 +62,6 @@ ActiveRecord::Schema.define(version: 2020_02_23_175837) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "companies", "users"
+  add_foreign_key "jobs", "companies"
 end
