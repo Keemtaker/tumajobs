168282032d2f905f532c3b06192f2a93a4a557bd
Hakeem Baba <keemtaker@yahoo.com>
2020-02-24 22:35:32 +0100

    setup of candidate model and companies controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,34 @@
 class CompaniesController < ApplicationController
+
+  def index
+     @companies = Company.all
+  end
+
+  def new
+     @company = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+    @company.user = current_user
+    @company.save
+  end
+
+  def show
+     @company = Company.find(params[:id])
+  end
+
+  def edit
+    @company = Company.find(params[:id])
+  end
+
+  def update
+    edit
+    @company.update(company_params)
+  end
+
+  def company_params
+    params.require(:company).permit(:name, :description, :website, :address, :location, :user_id)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_223611) do
+ActiveRecord::Schema.define(version: 2020_02_24_200814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "candidates", force: :cascade do |t|
+    t.string "full_name"
+    t.string "email"
+    t.string "mobile_number"
+    t.bigint "job_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["job_id"], name: "index_candidates_on_job_id"
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -62,6 +72,7 @@ ActiveRecord::Schema.define(version: 2020_02_23_223611) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "candidates", "jobs"
   add_foreign_key "companies", "users"
   add_foreign_key "jobs", "companies"
 end
