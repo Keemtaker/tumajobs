77da2bed3f8695821cffa36ab9299dd08b3dc401
Hakeem <babahakeem@outlook.com>
2020-03-26 09:22:16 +0100

    set up of job new and show

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,26 +1,48 @@
 class JobsController < ApplicationController
 
   def index
-    @jobs = Jobs.all
+    @jobs = Job.all
   end
 
-
   def new
-    @job = Job.new
+    @company = Company.find(params[:company_id])
+    @job = @company.jobs.new
   end
 
   def create
     @job = Job.new(job_params)
     @company = params[:company_id]
     @job.company_id = @company
-    @job.save
+    if @job.save
+      redirect_to company_job_path(@company, @job)
+    else
+      render :new
+    end
   end
 
   def show
     @job = Job.find(params[:id])
   end
 
-  def company_params
+  def edit
+    @company = Company.find(params[:company_id])
+    @job = Job.find(params[:id])
+
+  end
+
+  def update
+    edit
+    @company = params[:company_id]
+    @job.company_id = @company
+    @job.update(job_params)
+      if @job.update(job_params)
+        redirect_to company_job_path(@company, @job)
+      else
+        render :edit
+      end
+  end
+
+  def job_params
     params.require(:job).permit(:title, :description, :job_type, :keywords, :salary, :location, :company_id)
   end
 
