e0993cae3e3b31736d7ffa62255c7d9553dc82bf
Hakeem <babahakeem@outlook.com>
2020-04-12 21:40:31 +0200

    progress with jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,13 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
 
 require("bootstrap/dist/js/bootstrap")
+require("jquery")
+window.jQuery = $;
+window.$ = $;
+
+
 
 require("../perk")
 require("../search")
--- app/javascript/search.js
+++ app/javascript/search.js
@@ -1,4 +1,8 @@
-$(".jobs_search").keyup(function() {
+$(".jobs_search input").keyup(function() {
   $.get($(".jobs_search").attr("action"), $(".jobs_search").serialize(), null, "script");
     return false;
   });
+
+
+
+
