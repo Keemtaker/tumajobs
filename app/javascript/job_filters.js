47709afbd86dff0b5ab23f216bb52b7bbedf3507
Hakeem <babahakeem@outlook.com>
2020-04-24 18:41:00 +0200

    javascript pack tag works now in html head

--- app/javascript/job_filters.js
+++ app/javascript/job_filters.js
@@ -1,4 +1,6 @@
-$('input[type="checkbox"]').change(function() {
-    $.get($(".filters_click").attr("action"), $(".filters_click").serialize(), null, "script");
-    return false;
-});
+$(document).on('turbolinks:load', function(){
+  $('input[type="checkbox"]').change(function() {
+      $.get($(".filters_click").attr("action"), $(".filters_click").serialize(), null, "script");
+      return false;
+  });
+})
--- app/javascript/search.js
+++ app/javascript/search.js
@@ -1,8 +1,6 @@
-$(".jobs_search input").keyup(function() {
-  $.get($(".jobs_search").attr("action"), $(".jobs_search").serialize(), null, "script");
+$(document).on('turbolinks:load', function(){
+  $(".jobs_search input").keyup(function() {
+    $.get($(".jobs_search").attr("action"), $(".jobs_search").serialize(), null, "script");
     return false;
   });
-
-
-
-
+})
