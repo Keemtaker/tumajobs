0eec09c95a2d303940b5f508784fc358bb7f8983
Hakeem <babahakeem@outlook.com>
2020-04-28 19:25:24 +0200

    added navbar links for signed_in users

--- app/javascript/perk.js
+++ app/javascript/perk.js
@@ -1,5 +1,7 @@
-$(document).ready(function(){
-  $(".perk-choice").click(function(){
-    $(this).toggleClass("active");
+$(document).on('turbolinks:load', function(){
+  $(document).ready(function(){
+    $(".perk-choice").click(function(){
+      $(this).toggleClass("active");
+    });
   });
-});
+})
