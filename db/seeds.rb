f376f5963c3729ac81284594af252d3844fc8eaf
Hakeem <babahakeem@outlook.com>
2020-04-30 19:45:35 +0200

    perks seed for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,17 +30,46 @@
 
 # Perk.create!(perk_list)
 
-p "creating Jobs"
+# p "creating Jobs"
 
-description = Job.find(4).description
-company = Company.find(58)
+# description = Job.find(4).description
+# company = Company.find(58)
 
-job_list = [
-  {title: "Customer Success Consultant", description: description, role: "Customer Service", location: "Copenhagen", job_type: "Full-time", salary: "Paid", company_id: 58 },
-  {title: "Software Engineer", description: description, role: "Software & IT", location: "Tamale", job_type: "Part-time", salary: "Commission", company_id: 58 },
-  {title: "Sales Expert", description: description, role: "Marketing & Sales", location: "Accra", job_type: "Internship", salary: "Unpaid", company_id: 58 },
-  {title: "Project Manager", description: description, role: "Project Management", location: "Kumasi", job_type: "Full-time", salary: "Paid", company_id: 58 },
-  {title: "Agric Officer", description: description, role: "Other", location: "Copenhagen", job_type: "Full-time", salary: "Paid", company_id: 58 }
+# job_list = [
+#   {title: "Customer Success Consultant", description: description, role: "Customer Service", location: "Copenhagen", job_type: "Full-time", salary: "Paid", company_id: 58 },
+#   {title: "Software Engineer", description: description, role: "Software & IT", location: "Tamale", job_type: "Part-time", salary: "Commission", company_id: 58 },
+#   {title: "Sales Expert", description: description, role: "Marketing & Sales", location: "Accra", job_type: "Internship", salary: "Unpaid", company_id: 58 },
+#   {title: "Project Manager", description: description, role: "Project Management", location: "Kumasi", job_type: "Full-time", salary: "Paid", company_id: 58 },
+#   {title: "Agric Officer", description: description, role: "Other", location: "Copenhagen", job_type: "Full-time", salary: "Paid", company_id: 58 }
+# ]
+
+# Job.create!(job_list)
+
+
+
+# Heroku seeds
+Perk.destroy_all
+
+p "Creating Perks"
+
+perk_list = [
+  {name: "Catering", icon: "fas fa-utensils fa-3x" },
+  {name: "Skill Development", icon: "far fa-star fa-3x" },
+  {name: "Flexible Work Hours", icon: "far fa-clock fa-3x" },
+  {name: "Career Progression", icon: "fab fa-black-tie fa-3x" },
+  {name: "Work life balance", icon: "fas fa-balance-scale fa-3x" },
+  {name: "Health Insurance", icon: "fas fa-heartbeat fa-3x" },
+  {name: "Games & Hangouts", icon: "fas fa-gamepad fa-3x" },
+  {name: "Occasional Remote Work", icon: "fas fa-globe fa-3x" },
+  {name: "Paid Vacation", icon: "fas fa-plane fa-3x" },
+  {name: "Pension Plan", icon: "fas fa-home fa-3x" },
+  {name: "Performance Bonus", icon: "far fa-money-bill-alt fa-3x" },
+  {name: "Free Coffee", icon: "fas fa-coffee fa-3x" },
+  {name: "Parental Leave", icon: "fas fa-baby fa-3x" },
+  {name: "Friday Bars", icon: "fa fa-beer fa-3x" },
+  {name: "Gym Membership", icon: "fas fa-running fa-3x" },
 ]
 
-Job.create!(job_list)
+Perk.create!(perk_list)
+
+p "Perks created successfully"
