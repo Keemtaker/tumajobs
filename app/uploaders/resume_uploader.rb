baa2f26de22cc11e35294a17e41265d38fc29b50
Hakeem <babahakeem@outlook.com>
2020-05-08 11:42:16 +0200

    set up of job application confirmation email

--- app/uploaders/resume_uploader.rb
+++ app/uploaders/resume_uploader.rb
@@ -38,7 +38,7 @@ class ResumeUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-    %w(pdf doc docx odt)
+    %w(pdf doc docx)
   end
 
   # Override the filename of the uploaded files:
