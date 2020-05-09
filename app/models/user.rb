93602e33a07c4dba293731df0847c26deb18dfe4
Hakeem <babahakeem@outlook.com>
2020-05-09 17:42:15 +0200

    set up of user welcome mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
+  after_create :send_user_account_confirmation
+
   has_one :company, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :timeoutable,
          :validatable
+
+  private
+    def send_user_account_confirmation
+       UserMailer.user_account_confirmation(self).deliver_now
+    end
+
 end
