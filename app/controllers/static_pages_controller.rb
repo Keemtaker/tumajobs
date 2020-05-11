42b48a8238fea8508c3a8f5f49bbc14f07a27b7a
Hakeem <babahakeem@outlook.com>
2020-05-11 17:49:49 +0200

    skip for about and pricing

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:about, :pricing]
+  skip_after_action :verify_authorized
+
 
   def about
   end
