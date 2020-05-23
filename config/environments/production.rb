5c8415accb9763f01a3ff64c9da30b0e2ddc4a1f
Hakeem <babahakeem@outlook.com>
2020-05-23 18:13:12 +0200

    mail-gun error fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,8 +40,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
-    api_key: ENV['MAIL_GUN_API_KEY'],
-    domain: ENV['MAIL_GUN_DOMAIN']
+    api_key: ENV['MAILGUN_API_KEY'],
+    domain: ENV['MAILGUN_DOMAIN']
   }
 
 
