60e375665b15837b2eaf53eb705cbdde41518af0
Hakeem <babahakeem@outlook.com>
2020-05-23 18:03:33 +0200

    mail-gun

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'abdulhakeembaba92@gmail.com'
+  default from: 'postmaster@sandboxb516164b4f9341fdbeb407ef1c871afe.mailgun.org'
   layout 'mailer'
 end
