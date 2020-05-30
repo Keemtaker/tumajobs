f559794f3e6bf1795d3d30851de46edf29e1c878
Hakeem <babahakeem@outlook.com>
2020-05-30 16:41:31 +0200

    set-up of job uploader and job folder on cloudinary

--- app/models/job.rb
+++ app/models/job.rb
@@ -7,19 +7,20 @@ class Job < ApplicationRecord
   has_many :job_perks
   has_many :perks, through: :job_perks, dependent: :destroy
   has_many :talents, dependent: :destroy
-
   has_rich_text :description
+  mount_uploader :unregistered_company_logo, JobUploader
 
-  # validates :title, presence: true
-  # validates :description, presence: true
-  # validates :role, presence: true
-  # validates :location, presence: true
-  # validates :job_type, presence: true
-  # validates :salary, presence: true
-  # validates :job_email, presence: true, if: -> { self.job_application_type == "Email" }, format: Devise.email_regexp
-  # validates :job_url, presence: true, if: -> { self.job_application_type == "Url" }
-  # validates :unregistered_company_name, presence: true, if: -> { unregistered_company_validation }
-  # validates :unregistered_company_email, presence: true, if: -> { unregistered_company_validation }, format: Devise.email_regexp
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :role, presence: true
+  validates :location, presence: true
+  validates :job_type, presence: true
+  validates :salary, presence: true
+  validates :job_email, presence: true, if: -> { self.job_application_type == "Email" }, format: Devise.email_regexp
+  validates :job_url, presence: true, if: -> { self.job_application_type == "Url" }
+  validates :unregistered_company_name, presence: true, if: -> { unregistered_company_validation }
+  validates :unregistered_company_email, presence: true, if: -> { unregistered_company_validation }, format: Devise.email_regexp
 
   private
 
