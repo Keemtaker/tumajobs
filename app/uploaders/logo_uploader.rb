d0d7adcf7fdfcefda850fed458d168eae22c4ac2
Hakeem <babahakeem@outlook.com>
2020-05-30 16:16:33 +0200

    set-up of logo uploader folder on cloudinary

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -18,6 +18,10 @@ class LogoUploader < CarrierWave::Uploader::Base
     config.cache_storage = :file
   end
 
+  def public_id
+    return "company/" + model.user_id.to_s
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
@@ -40,9 +44,9 @@ class LogoUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg png svg)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
