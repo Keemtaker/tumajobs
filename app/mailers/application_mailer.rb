cdc5e19a7244f8e740d4298a0d285fac8ef2ec8e
Hakeem <babahakeem@outlook.com>
2020-06-19 22:06:50 +0200

    sendgrid

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'postmaster@sandboxb516164b4f9341fdbeb407ef1c871afe.mailgun.org'
+  default from: 'info@tumajobs.com'
   layout 'mailer'
 end
